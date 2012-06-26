02981ae7badb446ed6751ae7f6af191ddc8167fc
Roy Haddad <roy@1099pro.com>
2012-06-26 08:08:20 -0700

    Added a DebugConsoleLogger, and a GenericLogger
    * DebugConsoleLogger prints its messages out to the debug console (but
      if the project is included through NuGet this won't be available,
      because the call to Debug.WriteLine gets compiled out in release
      mode).
    
    * GenericLogger will execute a function given to it for every log
      message.

---
+++ src/Log5/GenericLogger.cs
@@ -0,0 +1,38 @@
+﻿namespace Log5
+{
+    using System;
+
+    public class GenericLogger : Logger
+    {
+
+        /// <summary>
+        /// The function to send log strings to. If this is null,
+        /// then logging does nothing
+        /// </summary>
+        public Action<string> LogFunction { get; set; }
+
+
+        /// <summary>
+        /// Construct a generic logger, with the given log function
+        /// </summary>
+        /// <param name="logFunction">The function to call when logging</param>
+        public GenericLogger(Action<string> logFunction)
+        {
+            LogFunction = logFunction;
+        }
+
+
+        /// <summary>
+        /// The internal log function. Every public logging method
+        /// feeds into this function
+        /// </summary>
+        /// <param name="logLine">The log string</param>
+        protected override void LogInternal(string logLine)
+        {
+            if (LogFunction != null)
+            {
+                LogFunction(logLine);
+            }
+        }
+    }
+}
